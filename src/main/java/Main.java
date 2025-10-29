0cc735602741317bbc0d6ee8348a4eede5cb8cea
Axelco201 <axel.sangoku0@gmail.com>
2025-10-29 18:36:39 +0100

    Add color parameter to point and drawCircle methods
    Point and link

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,30 @@ import static com.raylib.Raylib.*;
 
 class Point {
 	int x, y;
-	public static void point(int x, int y) {
+	float radius;
+	public static void point(int x, int y, Color color) {
+		DrawCircle( x, y, 7.0f, color);
+	}
+	public static void drawCircle(int x, int y, float radius, Color color, Color backGroundColor) {
+		DrawCircle( x, y, radius - 3.0f, backGroundColor);
+		DrawCircle( x, y, radius, color);
+	}
+	public static void speed() {
 		
 	}
 }
 
 public class Main {
+	private static void link(Point pointA, Point pointB) {
+		DrawLine(pointA.x, pointA.y, pointB.x, pointB.y, BLACK);
+	}
+	
     public static void main(String args[]) {
+    	Point pointA = new Point();
+    	pointA.x = 460; pointA.y = 170;
+    	Point pointB = new Point();
+    	pointB.x = 500; pointB.y = 370;
+    	
         InitWindow(1080, 720, "Procedural Animation");
         SetTargetFPS(60);
         
@@ -20,7 +37,11 @@ public class Main {
             
             ClearBackground(RAYWHITE);
             
-            DrawText("Hello world", 190, 200, 20, BLACK);
+            pointA.point(pointA.x, pointA.y, RED);
+            
+            pointB.point(pointB.x, pointB.y, RED);
+            
+            link(pointA, pointB);
             
             EndDrawing();
         }
