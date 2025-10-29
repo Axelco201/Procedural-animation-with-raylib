618e7ea5639361ad29a5a5498d310eddba16e5cf
Axelco201 <axel.sangoku0@gmail.com>
2025-10-29 13:16:47 +0100

    Change window size and adjust drawing settings
    i change the window size and remove the 3D

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,27 @@
 import static com.raylib.Colors.*;
 import static com.raylib.Raylib.*;
 
+
+class Point {
+	int x, y;
+	public static void point(int x, int y) {
+		
+	}
+}
+
 public class Main {
     public static void main(String args[]) {
-        InitWindow(800, 450, "Demo");
+        InitWindow(1080, 720, "Procedural Animation");
         SetTargetFPS(60);
-        Camera3D camera = new Camera3D()
-                ._position(new Vector3().x(18).y(16).z(18))
-                .target(new Vector3())
-                .up(new Vector3().x(0).y(1).z(0))
-                .fovy(45).projection(CAMERA_PERSPECTIVE);
-
+        
         while (!WindowShouldClose()) {
-            UpdateCamera(camera, CAMERA_ORBITAL);
+        	
             BeginDrawing();
+            
             ClearBackground(RAYWHITE);
-            BeginMode3D(camera);
-            DrawGrid(20, 1.0f);
-            EndMode3D();
-            DrawText("Hello world", 190, 200, 20, VIOLET);
-            DrawFPS(20, 20);
+            
+            DrawText("Hello world", 190, 200, 20, BLACK);
+            
             EndDrawing();
         }
         CloseWindow();
