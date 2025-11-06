ac1114fb1f8c168fd6dab78404b8c4e4f141c3a2
Axelco201 <axel.sangoku0@gmail.com>
2025-11-06 18:14:20 +0100

    Update Main.java
    there is a snake now and i fix link function and i add a function for a point to follow the mouse.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import static com.raylib.Colors.*;
 import static com.raylib.Raylib.*;
 
+import com.raylib.Raylib;
+
 
 class Point {
 	public int x;
@@ -9,13 +11,16 @@ class Point {
 	public static void point(int posX, int posY, Color color) {
 		DrawCircle( posX, posY, 7.0f, color);
 	}
-	public static void drawCircle(int x, int y, float radius, Color color, Color backGroundColor) {
+	public static void Circle(int x, int y, float radius, Color color, Color backGroundColor) {
 		DrawCircle( x, y, radius - 3.0f, backGroundColor);
 		DrawCircle( x, y, radius, color);
 	}
 	public void speed(float speedX, float speedY) {
 		x += speedX + 0.5;
 		y += speedY + 0.5;
+	}
+	public void follow(Vector2 mouse) {
+		speed((mouse.x() - x)/30,(mouse.y() - y)/30);
 	}
 }
 
@@ -38,7 +43,7 @@ public class Main {
 			deltaY = pointB.y - pointA.y;
 		}
 		
-		if(Math.sqrt(deltaX * deltaX + deltaY * deltaY) > lenth + 5) {
+		if(Math.sqrt(deltaX * deltaX + deltaY * deltaY) > lenth) {
 			if(pointA.x > pointB.x) {
 				pointA.speed(-(float)((deltaX - Math.cos(Math.acos(deltaX/Math.sqrt(deltaX * deltaX + deltaY * deltaY)))* lenth)/2.0f), 0);
 				pointB.speed((float)((deltaX - Math.cos(Math.acos(deltaX/Math.sqrt(deltaX * deltaX + deltaY * deltaY)))* lenth)/2.0f), 0);
@@ -57,7 +62,7 @@ public class Main {
 			}
 			
 		}
-		else if(Math.sqrt(deltaX * deltaX + deltaY * deltaY) < lenth + 5) {
+		else if(Math.sqrt(deltaX * deltaX + deltaY * deltaY) < lenth) {
 			if(pointA.x > pointB.x) {
 				pointA.speed((float)(((Math.cos(Math.acos(deltaX/Math.sqrt(deltaX * deltaX + deltaY * deltaY)))* lenth) - deltaX)/2.0f), 0);
 				pointB.speed(-(float)(((Math.cos(Math.acos(deltaX/Math.sqrt(deltaX * deltaX + deltaY * deltaY)))* lenth) - deltaX)/2.0f), 0);
@@ -87,7 +92,16 @@ public class Main {
     	pointB.x = 430; pointB.y = 140;
     	Point pointC = new Point();
     	pointC.x = 420; pointC.y = 160;
-    	
+    	Point pointD = new Point();
+    	pointD.x = 430; pointD.y = 180;
+    	Point pointE = new Point();
+    	pointE.x = 390; pointE.y = 120;
+    	Point pointF = new Point();
+    	pointF.x = 430; pointF.y = 140;
+    	Point pointG = new Point();
+    	pointG.x = 420; pointG.y = 160;
+    	Point pointH = new Point();
+    	pointH.x = 430; pointH.y = 180;
     	
         InitWindow(1080, 720, "Procedural Animation");
         SetTargetFPS(60);
@@ -95,12 +109,26 @@ public class Main {
         while (!WindowShouldClose()) {
         	
         	// update
-        	pointA.speed(2, 0);
+        	
+        	// mouse position
+        	Vector2 mouse = Raylib.GetMousePosition();
+        	
+        	pointA.follow(mouse);
         	
         	// render
             BeginDrawing();
             
             ClearBackground(RAYWHITE);
+            
+            link(pointA, pointB, 10);
+            link(pointC, pointB, 10);
+            link(pointD, pointC, 10);
+            link(pointD, pointE, 10);
+            link(pointE, pointF, 10);
+            link(pointF, pointG, 10);
+            link(pointH, pointG, 10 );
+            
+            
             
             pointA.point(pointA.x, pointA.y, RED);
             
@@ -108,9 +136,16 @@ public class Main {
             
             pointC.point(pointC.x, pointC.y, RED);
             
-            link(pointA, pointB, 40);
-            link(pointC, pointB, 40);
-            link(pointA, pointC, 40);
+            pointD.point(pointD.x, pointD.y, RED);
+            
+            pointE.point(pointE.x, pointE.y, RED);
+            
+            pointF.point(pointF.x, pointF.y, RED);
+            
+            pointG.point(pointG.x, pointG.y, RED);
+            
+            pointH.point(pointH.x, pointH.y, RED);
+            
             
             EndDrawing();
         	
